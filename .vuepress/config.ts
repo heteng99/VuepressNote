01dc769fbced0f8aa4685b8f8d56bbe4c6f748c6
liamhu <liamhu@tencent.com>
2022-07-20 16:49:40 +0800

    fix: 修复部署异常

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,7 +3,7 @@ import type { DefaultThemeOptions } from 'vuepress'
 import recoTheme from 'vuepress-theme-reco'
 
 export default defineUserConfig({
-  title: '🈶casual title🈚️',
+  title: 'casual title',
   description: 'Just playing around',
   theme: recoTheme({
     style: '@vuepress-reco/style-default',
